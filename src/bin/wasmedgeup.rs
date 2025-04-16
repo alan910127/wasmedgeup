6cd1b102546ec280c6f23829b87ceccf1dbba075
alan910127 <70696274+alan910127@users.noreply.github.com>
2025-04-17 01:46:39 +0800

    feat: use bin layout support `-v` flags
    Signed-off-by: alan910127 <70696274+alan910127@users.noreply.github.com>

--- src/bin/wasmedgeup.rs
+++ src/bin/wasmedgeup.rs
@@ -1,18 +1,28 @@
 use clap::Parser;
+use tracing::Level;
 use wasmedgeup::cli::Cli;
 use wasmedgeup::cli::CommandExecutor;
 use wasmedgeup::prelude::*;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
-
     let cli = Cli::parse();
     let ctx = cli.context(Default::default());
+
+    init_tracing(cli.verbose);
 
     if let Some(command) = cli.commands {
         command.execute(ctx).await?;
     };
 
     Ok(())
+}
+
+fn init_tracing(verbosity: u8) {
+    let level = match verbosity {
+        0 => Level::INFO,
+        1 => Level::DEBUG,
+        2.. => Level::TRACE,
+    };
+    tracing_subscriber::fmt().with_max_level(level).init();
 }
